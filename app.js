0e9dc223ee815ccd44e7d269e5c49b82093edd5e
daniel <dmsolisa@gmail.com>
2019-10-21 20:02:25 -0600

    avanze

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-$( document ).ready(function() {
-    console.log('We have Jquery')
-});
+$("#btn").click(function () {
+    var li = document.createElement("li");
+    var input = $("#add").val();
+    li.textContent = input;
+    li.id = "li";
+    console.log(li);
+    $("#list").append(li);
+})
+$("#li").click(function () {
+    this.style.textDecoration = "line-through";
+})      
