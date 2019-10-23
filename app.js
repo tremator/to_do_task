ecd4729b1325e982b08ef1e81bc2825a5b05764e
bhernandeza <bhernandeza06@gmail.com>
2019-10-23 07:46:16 -0600

    Resolving li tag click event

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
+var counter = 0;
+
 $("#btn").click(function () {
     var li = document.createElement("li");
     var input = $("#add").val();
     li.textContent = input;
-    li.id = "li";
-    console.log(li);
+    counter = counter + 1;
+    li.id = counter;
     $("#list").append(li);
 })
-$("#li").click(function () {
+
+$('#list').on('click','li', function(event){
+    event.preventDefault();
+    console.log(event.currentTarget.id); // This is to get the element id, for future cases.
     this.style.textDecoration = "line-through";
-})      
+})
